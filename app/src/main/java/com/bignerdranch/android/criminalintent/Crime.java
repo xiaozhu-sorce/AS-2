e9cc42ca884f96cb32eded709217ae105d1bab46
xiaozhu-sorce <790362227@qq.com>
2020-12-09 23:32:01 +0800

    CriminalIntent(Chapter7)

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,4 +13,32 @@ public class Crime {
         mId=UUID.randomUUID();
         mDate=new Date();
     }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+            //创建一个新的fragment事务，执行一个fragment添加操作，然后提交该事务
+            //add当法有两个参数：容器视图资源ID和新创建的CrimeFragment。
+            //容器视图资源ID是定义在activity_caime.xml中的FrameLayout组件的资源ID。
+        }
     }
 }
